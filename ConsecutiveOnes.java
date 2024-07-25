2f21d31243f0803349d917d5195dd06628d14058
Shrinkhal <111522967+shrinkhal14@users.noreply.github.com>
2024-07-25 14:44:31 +0530

    Consecutive Ones

---
+++ ConsecutiveOnes.java
@@ -0,0 +1,19 @@
+public class ConsecutiveOnes {
+    public static void printBinString(int n, int lastPlace, String str) {
+        if (n == 0) {
+            System.out.println(str);
+            return;
+        }
+        
+        printBinString(n - 1, 0, str + "0");
+        
+        
+        if (lastPlace == 0) {
+            printBinString(n - 1, 1, str + "1");
+        }
+    }
+
+    public static void main(String[] args) {
+        printBinString(3, 0, "");
+    }
+}
